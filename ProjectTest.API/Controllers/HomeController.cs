49c9072e45b31469d6dbf018f67d5a5d49882fbb
jhonega88 <jhonega88@hotmail.com>
2021-09-18 19:59:44 -0500

    Update version

--- ProjectTest.API/Controllers/HomeController.cs
+++ ProjectTest.API/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ProjectTest.API.Controllers
         public string Get()
         {
             _logger.LogInformation("Initial Home Get");
-            return "Net Core 5.0 Running ... 08072021-v1.0.0 JENKINS AFORO255 18 Septiembre";
+            return "Net Core 5.0 Running ... 08072021-v1.0.0 JENKINS AFORO255 18 Septiembre v2";
         }
     }
 }
